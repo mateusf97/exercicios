a41710246bcfb76a8e17a4c2497320203e3704c5
Mateus Felipe <mateus_f97@hotmail.com>
2019-11-16 01:53:46 -0200

    Documentos

--- PROVA-2-PDS2/main.cpp
+++ PROVA-2-PDS2/main.cpp
@@ -1,22 +1,20 @@
 #include "usuario.h"
 #include "administrador.h"
 
-int main(int argc, char const *argv[]) {
+int main() {
   Administrador *admin = new Administrador();
-  // Usuario *user = new Usuario();
+  Usuario *user = new Usuario();
 
-  // user->Adicionar(1);
-  // user->Adicionar(2);
-  // user->Adicionar(3);
+  user->Adicionar(1);
 
+  admin->Adicionar(1);
+  admin->Adicionar(2);
   admin->Adicionar(3);
-  admin->Adicionar(4);
-  admin->Adicionar(5);
   admin->Adicionar(6);
 
-  admin->Remover(4);
+  admin->Remover(2);
 
-  // delete user;
+  delete user;
   delete admin;
 
   return 0;
